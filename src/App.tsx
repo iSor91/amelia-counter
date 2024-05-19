98142cf7e21e095b52396fd8e725cc4c3edae3f7
isor91 <ratkovszki.istvn@gmail.com>
2024-05-19 12:04:04 +0200

    add minimalist css

--- src/App.tsx
+++ src/App.tsx
@@ -20,15 +20,17 @@ function App() {
     setAmeliaAge(Math.abs(currentDate - ameliaBornDate))
   }, [currentDate])
 
-  return <div>
-    <p>{ameliaBornDate.toLocaleString('hu-HU', { timeZone: 'Europe/Warsaw' })}</p>
-    <p>{currentDate.toLocaleString('hu-HU', { timeZone: 'Europe/Warsaw' })}</p>
-    <p>{ameliaAge.toLocaleString()} ms</p>
-    <p>{Math.floor(ameliaAge / baseDivider).toLocaleString()} s</p>
-    <p>{Math.floor(ameliaAge / (baseDivider * 60)).toLocaleString()} m</p>
-    <p>{Math.floor(ameliaAge / (baseDivider * 60 * 60)).toLocaleString()} h</p>
-    <p>{Math.floor(ameliaAge / (baseDivider * 60 * 60 * 24)).toLocaleString()} days</p>
-    <p>{Math.floor(ameliaAge / (baseDivider * 60 * 60 * 24 * 7)).toLocaleString()} weeks</p>
+  return <div className='d-flex h-100 w-100 justify-content-center'>
+    <div className='h-100 d-flex flex-column justify-content-center'>
+      <p>{ameliaBornDate.toLocaleString('hu-HU', { timeZone: 'Europe/Warsaw' })}</p>
+      <p>{currentDate.toLocaleString('hu-HU', { timeZone: 'Europe/Warsaw' })}</p>
+      <p>{ameliaAge.toLocaleString()} ms</p>
+      <p>{Math.floor(ameliaAge / baseDivider).toLocaleString()} s</p>
+      <p>{Math.floor(ameliaAge / (baseDivider * 60)).toLocaleString()} m</p>
+      <p>{Math.floor(ameliaAge / (baseDivider * 60 * 60)).toLocaleString()} h</p>
+      <p>{Math.floor(ameliaAge / (baseDivider * 60 * 60 * 24)).toLocaleString()} days</p>
+      <p>{Math.floor(ameliaAge / (baseDivider * 60 * 60 * 24 * 7)).toLocaleString()} weeks</p>
+    </div>
   </div>
 
 }
