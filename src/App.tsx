d56ddbfe80c75160008b9598be9de5d87d0a257d
isor91 <ratkovszki.istvn@gmail.com>
2024-06-01 21:17:24 +0200

    Attempts to fix github pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router/AppRouter'
 
 const App = () => {
-  return <BrowserRouter>
+  return <BrowserRouter basename="/amelia-counter">
     <AppRouter />
   </BrowserRouter>
 }
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -5,10 +5,10 @@ export const AppRouter = () => {
 
   return (
     <Routes>
-      <Route path="amelia-counter/amelia" element={<MainView date={new Date(2024, 3, 14, 5, 21)} />} />
-      <Route path="amelia-counter/isor" element={<MainView date={new Date(1991, 5, 9, 5, 21)} />} />
-      <Route path="amelia-counter/julo" element={<MainView date={new Date(1990, 9, 8, 9, 30)} />} />
-      <Route path="*" element={<Navigate to={"amelia-counter/amelia"} />} />
+      <Route path="amelia" element={<MainView date={new Date(2024, 3, 14, 5, 21)} />} />
+      <Route path="isor" element={<MainView date={new Date(1991, 5, 9, 5, 21)} />} />
+      <Route path="julo" element={<MainView date={new Date(1990, 9, 8, 9, 30)} />} />
+      <Route path="*" element={<Navigate to={"amelia"} />} />
     </Routes>
   )
 }
